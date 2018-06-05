8ad4316064a32cdeaf1cf39a8ea96664a6eb2d3c
steven <s811056@gmail.com>
2018-06-05 20:29:52 +0800

    2018-6-5

--- arduino/delta_3axis_mytest/delta_3axis_mytest.ino
+++ arduino/delta_3axis_mytest/delta_3axis_mytest.ino
@@ -7,12 +7,12 @@
 const int stepPin[3] = {2, 4, 6};
 const int dirPin[3] = {3, 5, 7};
 
-int16_t Address[500][3];  //輸入位址的紀錄存放
+int16_t Address[100][3];  //輸入位址的紀錄存放
 int16_t AddressGap[3];  //輸入與前一次所在的距離差
 int16_t Max=0;  //運轉圈數
 int16_t P[3] = {0, 0, 0}; //本次輸入的存放位置
 uint16_t t; //運轉時間
-int16_t Test[3];
+int16_t Test1=0,Test2=0,Test3=0;
 double L_AC; //A到C點的距離
 double thetal[2]; //角BAC 與 直線AC與X的夾角
 double thetal_B[2]={0,0}; //B的角度
@@ -40,36 +40,65 @@ void setup()
 }
 
 void loop()
-{  
-  ctrl_deg(Test[0], Test[1], Test[2], t);
-  if(Serial.available())
-  {
-    Hi=Serial.read();
-    if(Hi=='0')
-    {
-      scara_reset();
+{ 
+  while(1)
+  {   
+    if(Serial.available())
+    { 
+      P[0] = Serial.parseInt();
+      P[1] = Serial.parseInt();
+      P[2] = Serial.parseInt();
+      t = Serial.parseInt();   
+      Hi=Serial.read();
+      L_AC = sqrt(pow(P[0], 2) + pow(P[1], 2));   //arduino的三角函數出來都是
+                                        //弧度，需要*180/PI
+      if(L_AC>30)
+      {
+        Serial.println("輸入錯誤");
+        break;                                    
+      }
+      if(Hi=='1')
+      {
+        
+      }
+      if(Hi=='0')
+      {
+        scara_reset();
+      }
+      ctrl_deg(P[0],P[1],P[2],t);       
+      break;     
     }
   }
+  
 }
 
 void ctrl_deg(int16_t T0, int16_t T1, int16_t T2, uint16_t Tt)
-{
-  P[0] = T0;
-  P[1] = T1;
-  P[2] = T2;
-  t = Tt;  
-  for(int i=0;i<3;i++)
-  {
-    Address[Max][i]=P[i];
-  }  
+{         
+    t = Serial.parseInt();
+    Serial.print("輸入座標為-");
+    for(int i=0;i<3;i++)
+    {
+      Serial.print(P[i]);
+      Serial.print(" ");  
+    }
+    Serial.println();  
+    for(int i=0;i<3;i++)
+    {
+      Address[Max][i]=P[i];
+    }   
+    delta_3axis(); 
+    Max++;
+      
+}
+
+void DegreeOutput()
+{      
   
-  delta_3axis();
-  Max++;
 }
 
 void delta_3axis()
 {   
-  L_AC = sqrt(pow(P[0], 2) + pow(P[1], 2));   //arduino的三角函數出來都是
+//  L_AC = sqrt(pow(P[0], 2) + pow(P[1], 2));   //arduino的三角函數出來都是
                                         //弧度，需要*180/PI
   thetal[0] = acos((L_AC / (2 * first_arm))) * 180 / PI;
   thetal_B[0] = (180 - (2 * thetal[0]));
@@ -78,8 +107,14 @@ void delta_3axis()
   {
     L_AC = sqrt(pow(Address[Max-1][0], 2) + pow(Address[Max-1][1], 2));
     thetal[0] = acos((L_AC / (2 * first_arm))) * 180 / PI;
-    thetal_B[1] = (180 - (2 * thetal[0]));
-  }  
+    thetal_B[1] = (180 - (2 * thetal[0]));        
+  }
+  Serial.print("thetal_A轉動");
+  Serial.print(thetal[0]);
+  Serial.println("度");  
+  Serial.print("thetal_B轉動");
+  Serial.print(thetal_B[0]);
+  Serial.println("度");  
 }
 
 void scara_reset()
@@ -114,7 +149,11 @@ void scara_reset()
 void DegreeTurn()
 {
   if(P[0]>0) //向右轉
-  {    
+  { 
+//    Wire.beginTransmission(1);
+//    Wire.write("A_Direction");    
+//    Wire.write(0);
+//    Wire.endTransmission();   
     digitalWrite(dirPin,LOW);
   }
   if(P[0]<0) //向左轉
@@ -136,8 +175,4 @@ void DegreeTurn()
   }
 }
 
-void DegreeOutput()
-{
-  
-}
 
--- arduino/delta_3axis_slaveTest/delta_3axis_slaveTest.ino
+++ arduino/delta_3axis_slaveTest/delta_3axis_slaveTest.ino
@@ -2,22 +2,44 @@
 
 #define SlaveAddress 1
 
+const int stepPin[3] = {2, 4, 6};
+const int dirPin[3] = {3, 5, 7};
+boolean A_Direction;
+double turn[2];
+String slave;
+
 void setup() 
 {
   Wire.begin(SlaveAddress);
-  Wire.onReceive(turn);
+  Wire.onReceive(DegreeTurn);
+  Serial.begin(9600);
   Serial.println("I2C--Salve1---test");
   
 }
 
 void loop() 
 {
- 
-
+  
+  
 }
 
-void turn()
-{
+void DegreeTurn(int test)
+{  
+  while(Wire.available())
+  {
+    Serial.println(slave);
+    if(slave=="A_Direction");
+    {
+      
+    }
+  }
   
+  for(int i=0;i<int(turn[1]);i++) //b點旋轉
+  {
+    digitalWrite(stepPin[1], HIGH);
+    delayMicroseconds(800);
+    digitalWrite(stepPin[1], LOW);
+    delayMicroseconds(800);
+  }
 }
 
