1cd735d074ad46ef65e790b3a0ed6dcdd52f659c
steven <s811056@gmail.com>
2018-06-11 21:01:01 +0800

    2018-6-11-home

---
+++ arduino/For-All-3axis-Slave-test/For-All-3axis-Slave-test.ino
@@ -0,0 +1,97 @@
+#include <Wire.h>
+
+//  AB節臂
+#define SlaveAddress 1
+
+const int stepPin=2;
+const int dirPin=3;
+
+int returnStatus;
+boolean reset1;
+boolean A_Direction;
+double turn[2];
+String slave[10];
+
+void setup() 
+{
+  Wire.begin(SlaveAddress);
+  Wire.onReceive(DegreeTurn);
+  Wire.onRequest(requestEvent);
+  Serial.begin(9600);
+  Serial.println("I2C--Salve1---test");
+  pinMode(reset1,INPUT);
+  
+}
+
+void loop()
+{
+  
+  
+}
+
+void DegreeTurn()
+{  
+  while(Wire.available())
+  {    
+    slave[0]=Wire.read();
+    Serial.println(slave[0]);
+    slave[1]=Wire.read();
+    Serial.println(slave[1]);
+//    if(slave == 0)
+//    {
+//      turnRight();
+//    }
+//    if(slave == 1)
+//    {
+//      turnLeft();
+//    }
+//    if(slave == 3)
+//    {
+//      resetFunction();
+//    }
+  }  
+  
+}
+
+void resetFunction()
+{
+  digitalWrite(dirPin,HIGH);
+  delay(30);
+  while(reset1 == LOW) //  處碰到極限開關
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+  digitalWrite(dirPin,LOW);
+  delay(30);
+  for(int i=0;i<18;i++) 
+  { /*  往極限開關反方向轉18圈，大約2.025度，如果精度是1/16，
+    為了讓手臂放開極限開關  */
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+  returnStatus =1;
+}
+
+void turnRight()
+{
+  
+}
+
+void turnLeft()
+{
+  
+}
+
+void requestEvent()
+{
+  if(returnStatus == 1)
+  {
+    Wire.write(1);
+  }
+}
+
